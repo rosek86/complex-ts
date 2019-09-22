d3422fa67101df2fff5b23b54b61d174bb29e813
Krzysztof Rosinski <krzysiek@jrdltd.co.uk>
2019-09-22 10:56:06 +0200

    cleanup

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Complex Numbers in Typescript
+
+```bash
+npm i
+npm run test
+npm run build
+npm run repl
+
+> C.i.pow(5).toString()
+> .exit
+```
--- repl.ts
+++ repl.ts
@@ -1,10 +1,7 @@
-import { Complex } from './Complex';
+import { Complex } from './src/Complex';
 import * as repl from 'repl';
-import * as ComplexJS from 'complex.js';
 
 const server = repl.start('> ');
+
 server.context.Complex = Complex;
 server.context.C = Complex;
-server.context.ComplexJS = ComplexJS;
-server.context.Cjs = ComplexJS;
-
